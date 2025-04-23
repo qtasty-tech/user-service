4123f7f469841e9103637f243a04787f8ccbcd6c
Thisal Deelaka <165908864+ThisalDeelaka@users.noreply.github.com>
2025-04-23 10:33:31 +0530

    added G_Auth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const passport = require('passport');  
-const session = require('express-session');  
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middleware/authMiddleware');
-
-require('./middleware/passportConfig');  
 
 // Create the Express app
 const app = express();
@@ -15,24 +11,16 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,  
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session()); // Initialize Passport for authentication
-
 // Use authentication routes
 app.use('/api/auth', authRoutes);
 
-// Example of a protected route (only accessible by authenticated users)
+
 app.get('/api/protected', authMiddleware, (req, res) => {
   res.json({ message: 'You have access to this protected route!', user: req.user });
 });
 
-// MongoDB connection
+
+
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('MongoDB connected');
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,27 +2,24 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  // Get token from Authorization header
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  // If no token is found, return a 403 (Forbidden) response
   if (!token) {
     return res.status(403).json({ message: 'Access denied' });
   }
 
   try {
-    // Verify the token using the JWT_SECRET from .env
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Add the user info from the token to the request object
+    req.user = decoded;
 
     const email = decoded.email;
     if (!email.endsWith('@gmail.com')) {
       return res.status(400).json({ message: 'Please use a Gmail account to log in' });
     }
-    
-    next(); // Call the next middleware or route handler
+
+    next();
+
   } catch (error) {
-    // If the token is invalid or expired, return an error message
     res.status(400).json({ message: 'Invalid token' });
   }
 };
